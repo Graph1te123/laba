eac0618a98e3bf6b08ba526ba065ca07b8566c59
Graph1te123 <koeman884@gmail.com>
2025-10-03 18:48:27 +0300

    first commit

---
+++ src/main/java/com/example/HelloController.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, Spring Boot!";
+    }
+
+    @GetMapping("/greet")
+    public String greet(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello, %s!", name);
+    }
+}
